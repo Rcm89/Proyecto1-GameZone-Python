7ba6842e3781d037931edd6c73713973cf581833
Rcm89 <rcaste1989@gmail.com>
2024-10-07 02:00:09 +0200

    ahorcado con clases terminado y correciones en tres_en_raya

--- src/ahorcado.py
+++ src/ahorcado.py
@@ -1,155 +1,269 @@
 import random
 
-def obtener_palabra_aleatoria(lista_palabras):
-    """Selecciona aleatoriamente una palabra de la lista."""
-    return random.choice(lista_palabras)
-
-def dibujar_ahorcado(fallos):
-    """Dibuja el estado actual del ahorcado según el número de fallos."""
-    ahorcado_etapas = [
-        r"""
-           +---+
-           |   |
-               |
-               |
-               |
-               |
-        =========
-        """,
-        r"""
-           +---+
-           |   |
-           O   |
-               |
-               |
-               |
-        =========
-        """,
-        r"""
-           +---+
-           |   |
-           O   |
-           |   |
-               |
-               |
-        =========
-        """,
-        r"""
-           +---+
-           |   |
-           O   |
-          /|   |
-               |
-               |
-        =========
-        """,
-        r"""
-           +---+
-           |   |
-           O   |
-          /|\  |
-               |
-               |
-        =========
-        """,
-        r"""
-           +---+
-           |   |
-           O   |
-          /|\  |
-          /    |
-               |
-        =========
-        """,
-        r"""
-           +---+
-           |   |
-           O   |
-          /|\  |
-          / \  |
-               |
-        =========
+class Ahorcado:
+    def __init__(self):
         """
-    ] 
-    # Aquí nos aseguramos de que el índice no exceda el rango de la lista
-    if fallos < len(ahorcado_etapas):
-        print(ahorcado_etapas[fallos])
-    else:
-        print("¡ El número de fallos excede las etapas definidas!")
+        Método Constructor de la clase Ahorcado.
+        
+        Inicializa los atributos necesarios para el juego, incluyendo la lista de palabras,
+        el estado actual del juego (palabra seleccionada, letras intentadas, etc.).
+        """
+        # Lista de palabras posibles para el juego
+        self.palabras = [
+            "radio", "caballo", "motocicleta", "descampado", "algebra", "nube",
+            "cielo", "mesa", "silla", "libro", "gato", "papel", "coche",
+            "tren", "leche", "barco", "playa", "camion", "bosque", "ciudad",
+            "calle", "puente", "parque", "meseta", "biblioteca", "mariposa",
+            "elefante", "arrecife", "universidad", "amabilidad", "ventilador",
+            "arquitecto", "cascabel", "trastero", "tristeza", "naturaleza",
+            "estrategia", "gratificante", "pelota", "imprimir", "paraguas",
+            "nevera", "campo", "sostenible", "tranquilidad", "error",
+            "zapatillas", "corresponsal", "cordillera", "escuela"
+        ]
+        
+        self.palabra_seleccionada = ""
+        self.letras_intentadas = []
+        self.intentos_fallidos = 0
+        self.maximo_de_intentos = 6
+        self.tablero = []
+        self.jugar_de_nuevo = True
+    
+    def obtener_palabra_aleatoria(self):
+        """
+        Selecciona aleatoriamente una palabra de la lista de palabras.
 
-def juego_ahorcado():
-    """
-    Función principal que ejecuta el juego.
-    """
-    jugar_de_nuevo = True
-    # Lista de palabras que se emplean para el juego
-    palabras = [
-    "radio", "caballo", "motocicleta", "descampado", "algebra","nube", "cielo", "mesa", "silla",
-    "libro", "gato", "papel", "coche", "tren", "leche", "barco", "playa", "camion", "bosque",
-    "ciudad", "calle", "puente", "parque", "meseta", "biblioteca", "mariposa", "elefante", "arrecife",
-    "universidad", "amabilidad", "ventilador", "arquitecto", "cascabeles", "trastero", "tristeza",
-    "naturaleza", "estrategia", "gratificante", "pelota", "imprimir", "paraguas", "nevera", "campo"
-    "sostenible", "tranquilidad", "error", "zapatillas", "corresponsal", "cordillera", "escuela"]
-    while jugar_de_nuevo:
-        # Selecciona una palabra al azar de la lista
-        palabra_seleccionada = obtener_palabra_aleatoria(palabras)
-        letras_intentadas = []  # Lista para almacenar las letras que el jugador ha intentado
-        intentos_fallidos = 0   # Contador de intentos fallidos
-        maximo_de_intentos = 6        # Número máximo de intentos permitidos antes de perder
+        Returns:
+            str: La palabra seleccionada para la partida.
+        """
+        palabra = random.choice(self.palabras)
+        return palabra
+    
+    def dibujar_ahorcado(self):
+        """
+        Dibuja las diferentes etapas del ahorcado recogidas en la lista ahorcado_etaps.
+        """
+        ahorcado_etapas = [
+            r"""
+               +---+
+               |   |
+                   |
+                   |
+                   |
+                   |
+            =========
+            """,
+            r"""
+               +---+
+               |   |
+               O   |
+                   |
+                   |
+                   |
+            =========
+            """,
+            r"""
+               +---+
+               |   |
+               O   |
+               |   |
+                   |
+                   |
+            =========
+            """,
+            r"""
+               +---+
+               |   |
+               O   |
+              /|   |
+                   |
+                   |
+            =========
+            """,
+            r"""
+               +---+
+               |   |
+               O   |
+              /|\  |
+                   |
+                   |
+            =========
+            """,
+            r"""
+               +---+
+               |   |
+               O   |
+              /|\  |
+              /    |
+                   |
+            =========
+            """,
+            r"""
+               +---+
+               |   |
+               O   |
+              /|\  |
+              / \  |
+                   |
+            =========
+            """
+        ]
+        if self.intentos_fallidos < len(ahorcado_etapas):
+            print(ahorcado_etapas[self.intentos_fallidos])
+        
+    
+    def inicializar_partida(self):
+        """
+        Inicializa una nueva partida del Ahorcado.
 
-        # Crea una representación de la palabra con guiones bajos para las letras no adivinadas
-        tablero = ['_'] * len(palabra_seleccionada)
+        Selecciona una palabra aleatoria, resetea las letras intentadas, el contador de fallos y
+        configura el tablero con guiones bajos.
+        """
+        self.palabra_seleccionada = self.obtener_palabra_aleatoria()
+        self.letras_intentadas = []
+        self.intentos_fallidos = 0
+        self.tablero = ['_'] * len(self.palabra_seleccionada)
+        print('\n¡Bienvenido al juego del Ahorcado!')
+        print('Intenta acertar la palabra adivinando letra por letra.')
+    
+    def mostrar_estado_juego(self):
+        """
+        Muestra el estado actual del juego al jugador.
+        Incluye el dibujo del ahorcado, la palabra con las letras adivinadas y las letras que ya ha intentado.
+        """
+        self.dibujar_ahorcado()
+        # Mostrar la palabra con las letras adivinadas y guiones bajos
+        print('Palabra: ' + ' '.join(self.tablero))
+        # Mostrar las letras que el jugador ya ha intentado
+        print('Letras intentadas: ' + ', '.join(self.letras_intentadas))
+    
+    def solicitar_letra(self):
+        """
+        Solicita al jugador que introduzca una letra válida.
+        Verifica que la entrada sea una sola letra alfabética y que no haya sido intentada previamente.
 
-        print('¡Bienvenido al juego del Ahorcado!')
-
-        # Bucle principal del juego que continúa hasta que se cumpla una condición de fin
-        while intentos_fallidos < maximo_de_intentos and '_' in tablero:
-            # Muestra el estado actual del ahorcado y del tablero al jugador
-            dibujar_ahorcado(intentos_fallidos)
-            print('Palabra: ' + ' '.join(tablero))
-            print('Letras intentadas: ' + ', '.join(letras_intentadas))
-            # Solicita al jugador que ingrese una letra
+        Returns:
+            str: La letra válida ingresada por el jugador.
+        """
+        while True:
             letra = input('Introduce una letra: ').lower()
-
-            # Verifica que el jugador haya ingresado un solo carácter y que sea alfabético
+            # Verificar que solo se haya introducido un carácter alfabético
             if len(letra) != 1 or not letra.isalpha():
                 print('Por favor, introduce una sola letra.')
-                continue  # Vuelve al inicio del bucle para una nueva entrada
+                continue 
+            # Verificar si la letra ya ha sido intentada
+            if letra in self.letras_intentadas:
+                print('Ya has intentado esa letra. Intenta con otra.')
+                continue  # Volver al inicio del bucle
+            return letra
+    
+    def actualizar_tablero(self, letra):
+        """
+        Actualiza el tablero con la letra adivinada si está en la palabra.
 
-            # Verifica si la letra ya ha sido intentada anteriormente
-            if letra in letras_intentadas:
-                print('Ya has adivinado esa letra. Intenta con otra.')
-                continue  # Vuelve al inicio del bucle
-
-            # Añade la letra a la lista de letras intentadas
-            letras_intentadas.append(letra)
-            
-            # Comprueba si la letra está en la palabra seleccionada
-            if letra in palabra_seleccionada:
-                print(f'¡Muy bien! La letra "{letra}" está en la palabra.')
-                # Actualiza el tablero reemplazando los guiones bajos por la letra acertada
-                for i, l in enumerate(palabra_seleccionada):
-                    if l == letra:
-                        tablero[i] = letra
-            else:
-                # Incrementa el contador de intentos fallidos si la letra no está en la palabra
-                intentos_fallidos += 1
-                print(f'La letra "{letra}" no está en la palabra. Te quedan {maximo_de_intentos - intentos_fallidos} intentos.')
-
-        # Se muestra la etapa final del dibujo del ahorcado
-        dibujar_ahorcado(intentos_fallidos)
-
-        # Condición para determinar si el jugador ha ganado
-        if '_' not in tablero:
-            print('\n¡Felicidades! Has adivinado la palabra: ', palabra_seleccionada)
+        Args:
+            letra (str): La letra que el jugador ha intentado adivinar.
+        """
+        if letra in self.palabra_seleccionada:
+            print(f'¡Muy bien! La letra "{letra}" está en la palabra.')
+            # Reemplazar los guiones bajos por la letra acertada en las posiciones correctas
+            for indice, caracter in enumerate(self.palabra_seleccionada):
+                if caracter == letra:
+                    self.tablero[indice] = letra
         else:
-            # Condición para determinar si el jugador ha perdido
-            print('\nLo siento, has perdido. La palabra era: ', palabra_seleccionada)
-        # Preguntar al jugador si desea otra partida
-        respuesta = input("¿Quieres jugar otra vez? (s/n): ")
-        if respuesta.lower() != "s":
-            break
-    print("¡Gracias por jugar!. ¡Vuelve cuándo quieras!")
-# Punto de entrada del programa que inicia el juego
-if __name__ == '__main__':
-    juego_ahorcado()
+            # Incrementar el contador de intentos fallidos si la letra no está en la palabra
+            self.intentos_fallidos += 1
+            intentos_restantes = self.maximo_de_intentos - self.intentos_fallidos
+            print(f'La letra "{letra}" no está en la palabra. Te quedan {intentos_restantes} intentos.')
+    
+    def verificar_ganador(self):
+        """
+        Verifica si el jugador ha adivinado todas las letras de la palabra.
+
+        Returns:
+            bool: `True` si el jugador ha ganado, `False` en caso contrario.
+        """
+        return '_' not in self.tablero
+    
+    def verificar_perdedor(self):
+        """
+        Verifica si el jugador ha alcanzado el número máximo de intentos fallidos.
+
+        Returns:
+            bool: `True` si el jugador ha perdido, `False` en caso contrario.
+        """
+        return self.intentos_fallidos >= self.maximo_de_intentos
+    
+    def mostrar_final_juego(self):
+        """
+        Muestra las opciones al finalizar la partida y solicita al jugador que elija 
+        entre volver al menú principal o salir del juego.
+
+        Returns:
+            int: La opción seleccionada por el jugador (1 o 2).
+        """
+        print("\nElija una opción:")
+        print("1. Volver al menú principal")
+        print("2. Salir")
+        while True:
+            try:
+                seleccion_final = int(input("Su elección: "))
+                if seleccion_final not in [1, 2]:
+                    print("Por favor introduzca un 1 o un 2.")
+                else:
+                    break
+            except ValueError:
+                print("Por favor introduzca un 1 o un 2.")
+        return seleccion_final
+    
+    def jugar(self):
+        """
+        Controla el flujo principal del juego, permitiendo al jugador jugar múltiples partidas.
+        Maneja la inicialización de partidas, el ciclo de juego, la verificación de condiciones de fin,
+        y la decisión de jugar nuevamente o salir.
+        """
+        while self.jugar_de_nuevo:
+            self.inicializar_partida()
+            # Bucle principal de la partida
+            while not self.verificar_ganador() and not self.verificar_perdedor():
+                self.mostrar_estado_juego()
+                letra = self.solicitar_letra()
+                self.letras_intentadas.append(letra)
+                self.actualizar_tablero(letra)
+                print()  # Línea en blanco para mejorar la legibilidad
+            
+            # Mostrar el estado final del juego
+            self.dibujar_ahorcado()
+            if self.verificar_ganador():
+                print(f'\n¡Felicidades! Has adivinado la palabra: {self.palabra_seleccionada}')
+            else:
+                print(f'\nLo siento, has perdido. La palabra era: {self.palabra_seleccionada}')
+            
+            # Preguntar al jugador si desea jugar otra vez y si dice que no se le da a elegir
+            # entre volver al menu principal o salir del juego
+            respuesta = input("\n¿Quieres jugar otra vez? (s/n): ").lower()
+            if respuesta != "s":
+                seleccion_final = self.mostrar_final_juego()
+                if seleccion_final == 1:
+                    print("¡Gracias por jugar al Tres en Raya! Vuelve cuando quieras.")
+                    return seleccion_final
+                else:
+                    return seleccion_final
+                break
+
+"""
+if __name__ == "__main__":
+    juego = Ahorcado()
+    juego.jugar()     
+"""            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+
